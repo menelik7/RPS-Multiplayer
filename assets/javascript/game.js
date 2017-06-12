2a6a472b6d2f58b7e919773220f09255ab3aa6b5
Menelik Tefera <menelikworku@gmail.com>
2017-06-11 21:06:42 -0400

    some progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){ 
-  // Initialize Firebase
+   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyAgZ8gdQMcD3HoayHZWMrSs1FU6HmAnoEg",
     authDomain: "rock-paper-scissors-e52aa.firebaseapp.com",
@@ -16,43 +16,76 @@ $(document).ready(function(){
   var choices = ["rock", "paper", "scissors"];
 
   // Variables for tracking our wins, losses and ties. They begin at 0.
-  var playerOneName = "";
-  var playerTwoName = "";
-  var playerOneChoice = "";
-  var playerTwoChoice = "";
-  var playerOneWins = 0;
-  var playerTwoWins = 0;
-  var playerOneLosses = 0;
-  var playerTwoLosses = 0;
-  var ties = 0;
+  var players = [
+
+      player1 = {
+                playerOneName: "",
+                playerOneChoice: "",
+                playerOneWins: 0,
+                playerOneLosses: 0,
+                ties: 0
+                },
+
+      player2 = {
+                playerOneName: "",
+                playerTwoChoice: "",
+                playerTwoWins: 0,
+                playerTwoLosses: 0,
+                ties: 0
+                },
+  ];
+
+                var playerOneName = "";
+                var playerOneChoice = "";
+                var playerOneWins = 0;
+                var playerOneLosses = 0;
+                var playerTwoName = "";
+                var playerTwoChoice = "";
+                var playerTwoWins = 0;
+                var playerTwoLosses = 0;
+                var ties = 0;
 
   $("#add-name").on("click", function(event) {
       // prevent form from trying to submit/refresh the page
       event.preventDefault();
 
       // Capture User Inputs and store them into variables
-      var name = $("#name-input").val().trim(); //getting value and triming off extra space.
-      var email = $("#email-input").val().trim();
-      var age = $("#age-input").val().trim();
-      var comment = $("#comment-input").val().trim();
-
+      playerOneName = $("#name-input").val().trim(); //getting value and triming off extra space.
       // Console log each of the user inputs to confirm we are receiving them
-      console.log(name);
-      console.log(email);
-      console.log(age);
-      console.log(comment);
+      $("#player1-name").html(playerOneName);
+      $("#player1-wins").html("Wins: " + playerOneWins);
+      $("#player1-losses").html("Losses: " + playerOneLosses);
+      $("#ties1").html("Ties: " + ties);
+      $("#ties2").html("Ties: " + ties);
+      
 
-      // Output all of the new information into the relevant HTML sections
-      $("#name-display").html(name);
-      $("#email-display").html(email);
-      $("#age-display").html(age);
-      $("#comment-display").html(comment);
+      database.ref().set({
+        players: players
+      });
 
+      database.ref().on("value", function(snapshot) {
+
+      // Log everything that's coming out of snapshot
+      console.log(snapshot.val().playerOneName);
+      console.log(snapshot.val().playerOneChoice);
+      console.log(snapshot.val().playerOneWins);
+      console.log(snapshot.val().playerOneLosses);
+      console.log(snapshot.val().ties);
+
+      // Change the HTML to reflect
+      $("#player1-name").html(snapshot.val().playerOneName);
+      $("#player1-wins").html(snapshot.val().playerOneChoice);
+      $("#player1-wins").html(snapshot.val().playerOneWins);
+      $("#player1-losses").html(snapshot.val().playerOneLosses);
+      $("#ties1").html(snapshot.val().ties);
+
+      // Handle the errors
+    }, function(errorObject) {
+      console.log("Errors handled: " + errorObject.code);
     });
 
-  // When a player makes a choice by clicking on one of the choices, it will run the following function...
-  $("#xxx").on("click", function(event) {
-    
+
+
     // If the user presses "r" or "p" or "s", run the game logic.
     if ((playerOneChoice === "rock") || (playerOneChoice === "paper") || (playerOneChoice === "scissors")) {
 
